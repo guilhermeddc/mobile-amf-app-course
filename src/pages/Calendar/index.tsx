23d8a91b7215b8c78b6b2fd612828cbda0692fbf
Guilherme <guilhermeddc@gmail.com>
2020-08-02 15:06:02 -0300

    bundle criado

--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -1,11 +1,27 @@
 import React, {useCallback} from 'react';
+import {Linking} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {Feather as Icon} from '@expo/vector-icons';
 
-import {Container, CalendarView} from './styles';
+import {
+  Container,
+  CalendarView,
+  CalendarCard,
+  CalendarCardContent,
+  Image,
+  Title,
+  Date,
+} from './styles';
 
-import events from '../../utils/calendarEvents';
+import theme from '../../styles/theme';
 import Navigator from '../../components/Navigator';
-import CalendarCard from '../../components/CalendarCard';
+
+import image01 from '../../assets/events/1.png';
+import image02 from '../../assets/events/2.png';
+import image03 from '../../assets/events/3.png';
+import image04 from '../../assets/events/4.png';
+import image05 from '../../assets/events/5.png';
+import image06 from '../../assets/events/6.png';
 
 const Calendar: React.FC = () => {
   const {goBack} = useNavigation();
@@ -13,6 +29,10 @@ const Calendar: React.FC = () => {
   const handleNavigateBack = useCallback(() => {
     goBack();
   }, [goBack]);
+
+  const handleNavigateTolink = useCallback((url) => {
+    Linking.openURL(url);
+  }, []);
 
   return (
     <Container>
@@ -22,15 +42,117 @@ const Calendar: React.FC = () => {
         spotlight=" eventos"
       />
       <CalendarView>
-        {events?.map((item) => {
-          <CalendarCard
-            key={item.id}
-            onPress={() => {}}
-            title={item.title}
-            date={item.date}
-            // image={item.image}
-          />;
-        })}
+        <CalendarCard
+          onPress={() =>
+            handleNavigateTolink(
+              'https://faculdadeam.edu.br/noticias/detalhes/382/projeto-oikos-recanto-maestro-iniciara-coleta-oleo-usado-cozinha-para-descarte-consciente',
+            )
+          }>
+          <Image source={image01} />
+          <CalendarCardContent>
+            <Title>
+              Projeto OIKOS Recanto Maestro iniciará coleta de óleo usado de
+              cozinha para descarte consciente
+            </Title>
+
+            <Date>
+              <Icon name="calendar" color={theme.colors.cian} size={20} />
+              {'  '}sexta-feira, 31 de julho de 2020
+            </Date>
+          </CalendarCardContent>
+        </CalendarCard>
+
+        <CalendarCard
+          onPress={() =>
+            handleNavigateTolink(
+              'https://faculdadeam.edu.br/noticias/detalhes/381/amf--abre-edital-para-selecao-publica-docentes-para-o-curso-sistemas-informacao-',
+            )
+          }>
+          <Image source={image02} />
+          <CalendarCardContent>
+            <Title>
+              AMF abre edital para Seleção Pública de Docentes para o curso de
+              Sistemas de Informação
+            </Title>
+
+            <Date>
+              <Icon name="calendar" color={theme.colors.cian} size={20} />
+              {'  '}terça-feira, 28 de julho de 2020
+            </Date>
+          </CalendarCardContent>
+        </CalendarCard>
+
+        <CalendarCard
+          onPress={() =>
+            handleNavigateTolink(
+              'https://faculdadeam.edu.br/noticias/detalhes/380/recanto-maestro-e-noticia-no-jornal--diario--santa-maria-rs',
+            )
+          }>
+          <Image source={image03} />
+          <CalendarCardContent>
+            <Title>
+              Recanto Maestro é notícia no Jornal “Diário” de Santa Maria-RS
+            </Title>
+
+            <Date>
+              <Icon name="calendar" color={theme.colors.cian} size={20} />
+              {'  '}segunda-feira, 27 de julho de 2020
+            </Date>
+          </CalendarCardContent>
+        </CalendarCard>
+
+        <CalendarCard
+          onPress={() =>
+            handleNavigateTolink(
+              'https://faculdadeam.edu.br/noticias/detalhes/379/amf-ira-oferecer-o-curso-contabilidade-ja-no-proximo-vestibular',
+            )
+          }>
+          <Image source={image04} />
+          <CalendarCardContent>
+            <Title>
+              AMF irá oferecer o curso de Contabilidade já no próximo Vestibular
+            </Title>
+
+            <Date>
+              <Icon name="calendar" color={theme.colors.cian} size={20} />
+              {'  '}quinta-feira, 23 de julho de 2020
+            </Date>
+          </CalendarCardContent>
+        </CalendarCard>
+
+        <CalendarCard
+          onPress={() =>
+            handleNavigateTolink(
+              'https://faculdadeam.edu.br/noticias/detalhes/378/saber-humano-10-anos-',
+            )
+          }>
+          <Image source={image05} />
+          <CalendarCardContent>
+            <Title>Saber Humano 10 anos!</Title>
+
+            <Date>
+              <Icon name="calendar" color={theme.colors.cian} size={20} />
+              {'  '}quarta-feira, 22 de julho de 2020
+            </Date>
+          </CalendarCardContent>
+        </CalendarCard>
+
+        <CalendarCard
+          onPress={() =>
+            handleNavigateTolink(
+              'https://faculdadeam.edu.br/noticias/detalhes/377/amf-e-destaque-nos-resultados-da-prova-da-oab--',
+            )
+          }>
+          <Image source={image06} />
+          <CalendarCardContent>
+            <Title>AMF é destaque nos resultados da prova da OAB!</Title>
+
+            <Date>
+              <Icon name="calendar" color={theme.colors.cian} size={20} />
+              {'  '}quarta-feira, 24 de junho de 2020
+            </Date>
+          </CalendarCardContent>
+        </CalendarCard>
       </CalendarView>
     </Container>
   );
--- src/pages/Calendar/styles.ts
+++ src/pages/Calendar/styles.ts
@@ -8,7 +8,35 @@ export const Container = styled.View`
   justify-content: space-between;
 `;
 
-export const CalendarView = styled.View`
+export const CalendarView = styled.ScrollView`
   flex: 1;
   margin-top: 16px;
+`;
+
+export const CalendarCard = styled.TouchableOpacity`
+  width: 100%;
+  background: ${theme.colors.light};
+  margin-bottom: 16px;
+`;
+
+export const CalendarCardContent = styled.View`
+  padding: 16px;
+`;
+
+export const Image = styled.Image`
+  width: 100%;
+  height: 200px;
+`;
+
+export const Title = styled.Text`
+  color: ${theme.colors.cian};
+  font-family: ${theme.fonts.medium};
+  font-size: 18px;
+  margin-bottom: 8px;
+`;
+
+export const Date = styled.Text`
+  align-items: center;
+  font-family: ${theme.fonts.regular};
+  color: ${theme.colors.gray};
 `;
